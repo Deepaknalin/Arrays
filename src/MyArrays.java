d443c3f1e718fb27d2bb2e2b6506a685ca25ec37
Deepak Kr Nalin <mail2dknalin@gmail.com>
2019-02-14 23:21:49 +0530

    changes done

---
+++ src/MyArrays.java
@@ -0,0 +1,12 @@
+public class MyArrays {
+    public static void main(String[] args) {
+        int   [] var = new int[100];
+        var[0] = 123;
+        var[99] = 456;
+        System.out.println(var[1]);
+        System.out.println(var[99]);
+        System.out.println(var[54]);
+        System.out.println(var[24]);
+    }
+
+}
